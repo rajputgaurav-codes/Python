ef535215034a4e3a9138f756b445eefdf048fc8e
gauravsingh22-del <gr0757931@gmail.com>
2026-01-12 22:43:03 +0530

    first chapter

---
+++ chapter 1/program1.py
@@ -0,0 +1,4 @@
+print("hello world")
+"""
+dfsgkjlgjklfdjgkl;j
+"""
---
+++ practice set 1/third.py
@@ -0,0 +1,11 @@
+import os
+
+# specify the directory you want to list
+directory_path = "/"  # change this to any folder you want
+
+# get list of files and folders
+contents = os.listdir(directory_path)
+
+# print each item
+for item in contents:
+    print(item)
