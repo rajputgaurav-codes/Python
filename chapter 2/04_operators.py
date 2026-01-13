2520841cf28273044abd9ac7ef461c4822952138
gauravsingh22-del <gr0757931@gmail.com>
2026-01-13 22:30:03 +0530

    operators

---
+++ chapter 2/04_operators.py
@@ -0,0 +1,27 @@
+# Arithmatic operators
+
+a=2
+b=4
+c=a+b
+print(c)
+
+# Assignment operators
+a=4-2 # assign 4-2 in variable a
+b=5
+b+=4 # increment value b by 4 and assign in b
+
+# comaprision operatotr
+
+a=5>4
+print(a)
+b=3!=4
+print(b)
+
+# logical operators
+
+a=True or False
+print(a)
+b=True and False
+print(b)
+
+print(not(True))
