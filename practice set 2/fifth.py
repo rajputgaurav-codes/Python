de6ab124cf632fe64d0facd333c82262ca5c5a66
gauravsingh22-del <gr0757931@gmail.com>
2026-01-13 22:37:53 +0530

    find an average of two numbers enterd by the user.

---
+++ practice set 2/fifth.py
@@ -0,0 +1,5 @@
+# write a python program to find an average of two numbers enterd by the user.
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=(a+b)/2
+print("Average is:",c)
