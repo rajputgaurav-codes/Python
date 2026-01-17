ec9afaf2c9580c0228dfd951c4760add47d13268
gauravsingh22-del <gr0757931@gmail.com>
2026-01-17 18:50:33 +0530

    display a user enterd name followed by good afternoon using input function

---
+++ practice set 3/01.py
@@ -0,0 +1,5 @@
+# write a program to display a user enterd name followed by good afternoon using input function.
+name=input("enter a name:")
+print("Good Afternoon",name)
+
+print(f"Good Afternoon {name}") # using f string
