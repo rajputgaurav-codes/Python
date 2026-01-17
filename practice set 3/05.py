ac9c8021c6fcdd911b375d8fdf9f7e348c7781a2
gauravsingh22-del <gr0757931@gmail.com>
2026-01-17 18:52:57 +0530

    using escape sequence

---
+++ practice set 3/05.py
@@ -0,0 +1,4 @@
+#write a program to format the following letter using escape sequence character.
+#Dear gaurav,this python course is nice.Thanks!
+
+print("Dear gaurav,\nthis python course is nice.\nThanks!") #using escape seuence: \n
