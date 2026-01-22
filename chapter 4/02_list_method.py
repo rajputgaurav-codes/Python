df1c251dc483f5cbb96cffc087132c8c1d021d6d
gauravsingh22-del <gr0757931@gmail.com>
2026-01-22 20:22:11 +0530

    learn different method of list

---
+++ chapter 4/02_list_method.py
@@ -0,0 +1,13 @@
+# friends=["apple",23,'hello',4.234,'t','#$','gaurav']
+# print(friends[0])
+# friends.append(22222) #use the append method
+# print(friends)
+
+l1=[25,24,67,1,5,63,98,45]
+# l1.sort() using sort method
+# l1.reverse() using reverse method
+# l1.insert(4,45454) using the insert method to add new value
+# print(l1.pop(2)) # using pop method to use to delete the particular value
+l1.remove(98)
+print(l1)
+
