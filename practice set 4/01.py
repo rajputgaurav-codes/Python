ad6237ae11eeb593d7b74f49d2a6097c298e828a
gauravsingh22-del <gr0757931@gmail.com>
2026-04-11 22:33:43 +0530

    write a program to store seven fruites name enter by the user

---
+++ practice set 4/01.py
@@ -0,0 +1,20 @@
+# write a program to store seven fruites name enter by the user.
+
+fruits=[]
+
+f1=input("enter the first fruits name : ")
+fruits.append(f1)
+f2=input("enter the second fruits name : ")
+fruits.append(f2)
+f3=input("enter the third fruits name : ")
+fruits.append(f3)
+f4=input("enter the fourth fruits name : ")
+fruits.append(f4)
+f5=input("enter the fifth fruits name : ")
+fruits.append(f5)
+f6=input("enter the sixth fruits name : ")
+fruits.append(f6)
+f7=input("enter the seventh fruits name : ")
+fruits.append(f7)
+
+print(fruits)
