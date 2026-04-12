b30b580c9b51e0c4097eb0180b02f1607e96d0f5
gauravsingh22-del <gr0757931@gmail.com>
2026-04-12 19:40:16 +0530

    write a pragram to accep the marks of six student and shorted in manner

---
+++ practice set 4/02.py
@@ -0,0 +1,19 @@
+# write a pragram to accep the marks of six student and shorted in manner.
+marks=[]
+
+m1=input("enter the first student marks  : ")
+marks.append(m1)
+m2=input("enter the second student marks  : ")
+marks.append(m2)
+m3=input("enter the third student marks  : ")
+marks.append(m3)
+m4=input("enter the fourth student marks : ")
+marks.append(m4)
+m5=input("enter the fifth student marks: ")
+marks.append(m5)
+m6=input("enter the sixth student  marks  : ")
+marks.append(m6)
+
+marks.sort()
+
+print(marks)
