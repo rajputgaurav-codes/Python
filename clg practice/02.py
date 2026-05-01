3ac165f781919514730a5b98ed3289211a16f17b
gauravsingh22-del <gr0757931@gmail.com>
2026-05-01 20:41:36 +0530

    Write a python program to find the square root of a number.

---
+++ clg practice/02.py
@@ -0,0 +1,9 @@
+# Write a Python program to find the square root of a number. 
+num= float(input("enter a number : "))
+sqrt= num ** 0.5
+print("Square root of number is : ",sqrt)
+
+# using math build in function
+import math
+num= float(input("enter a number : "))
+print("square root of a number : ",math.sqrt(num))
