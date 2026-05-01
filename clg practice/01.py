7542d882b01075b9f38a5dadbceac544c1680948
gauravsingh22-del <gr0757931@gmail.com>
2026-05-01 20:38:12 +0530

    Write a python program to display reverse number

---
+++ clg practice/01.py
@@ -0,0 +1,15 @@
+#Write a python program to display reverse number. 
+
+# l1= input("enter the number : ")
+# rev= l1[::-1]
+# print("reverse number :",int(rev))
+
+#using formula/logic
+num=int(input("enter the number : "))
+reverse=0
+while num > 0:
+    digit=num%10
+    reverse=reverse*10+digit
+    num=num//10
+print("reverse number is : ",reverse)
+
