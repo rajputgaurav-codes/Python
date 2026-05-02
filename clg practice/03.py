cf42679a072db3b61f49b2f7944f8651f20c5930
gauravsingh22-del <gr0757931@gmail.com>
2026-05-02 17:24:53 +0530

    write a python to find the area of a triangle.

---
+++ clg practice/03.py
@@ -0,0 +1,6 @@
+#Write a Python program to find the area of a triangle. 
+
+h=int(input("Enter the value of hight : "))
+b=int(input("Enter the value of breath : "))
+area_of_triangle=0.5*(h*b)
+print("Area of triangle is : ",area_of_triangle)
