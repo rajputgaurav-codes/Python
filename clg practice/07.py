e886bb771c480bcf320b6ffa139c364caafa87a2
gauravsingh22-del <gr0757931@gmail.com>
2026-05-05 15:49:13 +0530

    write a python program to find wherher a number is positive , negative or zero.

---
+++ clg practice/07.py
@@ -0,0 +1,8 @@
+#Write a Python program to find whether a number is positive, negative or zero. 
+num=int(input("Enter a number"))
+if(num>0):
+    print("It is a positive number")
+elif(num<0):
+    print("It is a negative number")
+else:
+    print("Zero")
