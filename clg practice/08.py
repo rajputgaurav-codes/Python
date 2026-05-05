4564c0efe9f78766e5943bf2ae84db46ff6dba8b
gauravsingh22-del <gr0757931@gmail.com>
2026-05-05 15:50:19 +0530

    write a python program to print the absolute number.

---
+++ clg practice/08.py
@@ -0,0 +1,8 @@
+#print the absolute number.
+num=int(input("Enter a number : "))
+if(num<0):
+    absolute_number= -num
+    print("absolute number: ",absolute_number)
+else:
+    absolute_number= num
+    print("absolute number: ",absolute_number)
