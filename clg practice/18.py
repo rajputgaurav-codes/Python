130e5aaaadde5330d3062e0c12d0bfc9276573d5
gauravsingh22-del <gr0757931@gmail.com>
2026-05-14 05:08:02 +0530

    Write a python program that performs the following

---
+++ clg practice/18.py
@@ -0,0 +1,10 @@
+'''Write a Python program that performs the following: 
+       Ex. Inputted String: Hello Technocrates 
+1. Print all character by skipping 1 character. 
+2. Print entire string reverse 
+3. Print length of the String.'''
+name="Hello Technocrates "
+print("skipping 1 character: ",name[::2])
+print("string reverse : ",name[::-1])
+print("length of the String: ",len(name))
+
