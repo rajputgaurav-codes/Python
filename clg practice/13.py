da65ec7ab7d89d3a6ea9b6b8dad29d9730bda954
gauravsingh22-del <gr0757931@gmail.com>
2026-05-13 17:59:51 +0530

    Write a python program to count total number of times a specified value appears in given string.

---
+++ clg practice/13.py
@@ -0,0 +1,5 @@
+'''Write a python program to count total number of times a Specified value appears in 
+given string.'''
+name="gaurav is a good boy and also it is intaligent"
+count=input("enter a value to a count: ")
+print(name.count(count))
