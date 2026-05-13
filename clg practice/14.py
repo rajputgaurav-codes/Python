6961b1740bb58440ebf157c1441079d6815f53ca
gauravsingh22-del <gr0757931@gmail.com>
2026-05-13 18:00:55 +0530

    Write a python program to add and remove element from the list.

---
+++ clg practice/14.py
@@ -0,0 +1,6 @@
+'''Write a python program to add and remove element from the list'''
+l1=["gaurav","ravi",45,85,67,"rohan","kamal"]
+l1.append(33333)
+print("After add: ",l1)
+l1.remove("rohan")
+print("After remove: ",l1)
