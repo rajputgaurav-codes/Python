4b3f71d4880639669d6b2276567f0f067153a01a
gauravsingh22-del <gr0757931@gmail.com>
2026-05-15 18:36:18 +0530

    Write a python program that performs the following

---
+++ clg practice/19.py
@@ -0,0 +1,9 @@
+'''Write a Python program that performs the following: 
+1. Sort the list 
+2. Display sum of the all elements of the list. 
+3. Display last element of the list. '''
+l1=[45,24,33,44,25,78,54,35]
+l1.sort()
+print("Sort the list:=",l1)
+print("Sum of all elements of the list: ",sum(l1))
+print("last element of the list: ",l1[-1])
