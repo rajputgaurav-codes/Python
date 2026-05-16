f0b9ffa7d3e77dd9d586ee4715ea82e8a6ef29df
gauravsingh22-del <gr0757931@gmail.com>
2026-05-17 04:47:58 +0530

    Write a python program to find the avg of the two number take input from the both number from user

---
+++ clg practice/27.py
@@ -0,0 +1,6 @@
+#write a python program to find the avg of the two number take input from the both number from user.
+a=int(input("enter a first number :"))
+b=int(input("enter a second number :"))
+c=a+b
+d=c/2
+print("The avg of both number is:",d)
